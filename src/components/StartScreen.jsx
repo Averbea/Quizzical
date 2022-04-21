d2ab07497bf1bf68dcf1e08288e0f9d3245b8c65
Alexander Averbeck <aa232442@fh-muenster.de>
2022-04-21 19:31:13 +0200

    start-screen implementation

---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,11 @@
+import React  from "react";
+
+export default function StartScreen(){
+    return(
+        <div className="start-screen">
+            <h1>Quizzical</h1>
+            <p>Try to solve these Random Questions</p>
+            <button>Start Quiz</button>
+        </div>
+    )
+}
